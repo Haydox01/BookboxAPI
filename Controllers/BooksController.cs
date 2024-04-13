45036f95bd0943cb6380429d75706b51e11ed238
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-04-13 10:26:53 +0100

    Completed working on Book Repository

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Bookbox.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookbox.Controllers
@@ -7,9 +9,19 @@ namespace Bookbox.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        public BooksController()
+        private readonly IMapper mapper;
+        private readonly IBookRepository bookRepository;
+
+        public BooksController(IMapper mapper, IBookRepository bookRepository)
         {
-            
+            this.mapper = mapper;
+            this.bookRepository = bookRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books = await bookRepository.GetAllBooks();
+            return Ok(books);
         }
     }
 }
